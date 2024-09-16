04cdca5bb9e38cf94e096981f910286b618cbd3e
Trizah75 <trizahdanson75@gmail.com>
2024-09-16 12:12:52 +0000

    general updates and enhancements

--- src/script.js
+++ src/script.js
@@ -1,12 +1,23 @@
-function generateAffirmation(event) {
-  event.preventDefault();
-
+function displayAffirmation(response) {
   new Typewriter("p", {
-    strings: "You are loved and strong! Keep embracing.",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 10,
   });
+}
+
+function generateAffirmation(event) {
+  event.preventDefault();
+
+  let instructions = document.querySelector("#instructions");
+  let apiKey = "oc8ctbd8ae9cf7802f361094f4eae69b";
+  let prompt = `Create a four line daily affirmation specifically about ${instructions.value}.Keep it motivational, positive and short`;
+  let context =
+    "You are creating four-line affirmations based on the user's chosen topic.The affirmations should be brief, positive and uplifiting. Focus on empowering and motivation the user with a clear connection to the chosen topic.Keep each affirmation concise but impactful.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayAffirmation);
 }
 let affirmationForm = document.querySelector("#affirmation-form");
 affirmationForm.addEventListener("submit", generateAffirmation);
